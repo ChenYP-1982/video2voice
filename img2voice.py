0ba499be48197cd3bce3dff729a58ae45b53e457
Chen Yen Pin <chenyenpin1982@gmail.com>
2024-03-03 21:21:57 -0300

    first commit

---
+++ img2voice.py
@@ -0,0 +1,43 @@
+from dotenv import find_dotenv, load_dotenv
+import streamlit as st
+import requests
+import os
+
+
+
+from transformers import pipeline
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# Load .envfile
+load_dotenv(find_dotenv())
+Huggingface_api_token=os.getenv("Huggingface_api_token")
+
+# gerando texto para imagem 
+
+def img2text(path):
+    img_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+
+    text= img_to_text(path)[0]["generated_text"]
+    
+    print(text)
+
+    return text
+
+
+# converting text to speech
+def text2speedch(text, speed=1):
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": f"Bearer {Huggingface_api_token}"}
+    payloads={
+        "inputs": text
+    }
+
+    response=requests.post(API_URL,headers=headers,json=payloads)
+    with open("audio.wav", "wb")as f:
+        f.write(response.content)
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Creating Virtual enviroment
+``` bash ```
+
+conda create -n image2voice python=3.9
+
+conda activate image2voice
+
+pip install requirements.txt
+
+conda deactivate
+
